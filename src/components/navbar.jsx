aa10a0e7ab29403944da18fa38955ae97b6a6b43
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-11-12 17:17:00 +0000

    added a navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+class NavbarComponent extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="/">Dividend Tracker</Navbar.Brand>
+                    <Nav className="mr-auto">
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default NavbarComponent;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from '../components/navbar';
 
 class Home extends Component {
     state = {}
     render() {
         return (
             <div>
-
+                <Navbar />
             </div>
         );
     }
